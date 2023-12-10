d3b59b401d7dcde85e268f312a89c8d4663c3534
Marcin Torba <torbamarcin@gmail.com>
2023-12-10 15:14:38 +0100

    Initial version (#1)
    * Initial version
    
    * Fix naming
    
    * Update pipeline

---
+++ src/Mako-IoT.Device.Services.Logging.Sinks.SdCard/IocExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MakoIoT.Device.Services.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakoIoT.Device.Services.Logging.Sinks.SdCard
+{
+    public static class IocExtensions
+    {
+        public static void AddSdCardSink(this IServiceCollection serviceCollection, SdCardSinkConfig config)
+        {
+            var consoleSink = new SdCardSink(config);
+            serviceCollection.AddSingleton(typeof(ILogSink), consoleSink);
+        }
+    }
+}
---
+++ src/Mako-IoT.Device.Services.Logging.Sinks.SdCard/SdCardSinkConfig.cs
@@ -0,0 +1,11 @@
+﻿
+using static nanoFramework.System.IO.FileSystem.SDCard;
+
+namespace MakoIoT.Device.Services.Logging.Sinks.SdCard
+{
+    public sealed class SdCardSinkConfig
+    {
+        public SDCardSpiParameters sdCardSpi { get; set; }
+        public string FileName { get; set; }
+    }
+}
